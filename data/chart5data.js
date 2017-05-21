9530a94abf2082c75fd13ecc936c56f58aef3cf5
Brian Sweeney <bsweeney930@gmail.com>
2017-05-21 16:16:30 -0400

    updated chart 5 data

--- data/chart5data.js
+++ data/chart5data.js
@@ -1,16 +1,26 @@
 var chart5data = [
-  {y: 1.360530419036175, x: 1.092559, country: 'AUS', labelY: 'center', labelX: 'left'},
-  {y: 1.29399312764886, x: 1.097533, country: 'UK', labelY: 'top', labelX: 'right'},
-  {y:  1.251787757074009, x: 1.107637, country: 'NETH', labelY: 'center', labelX: 'right'},
-  {y: 1.151665180612727, x: 1.094083, country: 'NZ', labelY: 'center', labelX: 'left'},
-  {y: 1.13376085186227, x: 1.099346, country: 'NOR', labelY: 'top', labelX: 'right'},
-  {y: 1.0674114551491452, x: 1.110704, country: 'GER', labelY: 'center', labelX: 'left'},
-  {y: 1.0675352522899197, x: 1.115361, country: 'SWIZ', labelY: 'center', labelX: 'right'},
-  {y: 1.064104826992644, x: 1.110839, country: 'SWE', labelY: 'bottom', labelX: 'center'},
-  {y: 1-0.247553882107697, x: 1.101473, country: 'CAN', labelY: 'center', labelX: 'right'},
-  {y: 1-0.41705654741814, x: 1.109881, country: 'FRA', labelY: 'center', labelX: 'right'},
-  {y: 1-0.706030660985181, x: 1.169148, country: 'US', labelY: 'center', labelX: 'right'},
+  {y: 1.357430028143254, x: 1.092559, country: 'AUS', labelY: 'center', labelX: 'left'},
+  {y: 1.367418713525976, x: 1.097533, country: 'UK', labelY: 'top', labelX: 'right'},
+  {y: 1.272365608999781, x: 1.107637, country: 'NETH', labelY: 'center', labelX: 'right'},
+  {y: 1.132375052466037, x: 1.094083, country: 'NZ', labelY: 'center', labelX: 'left'},
+  {y: 1.121808425645605, x: 1.099346, country: 'NOR', labelY: 'top', labelX: 'right'},
+  {y: 1.0836445458412802, x: 1.110704, country: 'GER', labelY: 'center', labelX: 'left'},
+  {y: 1.0801972684432023, x: 1.115361, country: 'SWIZ', labelY: 'center', labelX: 'right'},
+  {y: 1.0735158050805369, x: 1.110839, country: 'SWE', labelY: 'bottom', labelX: 'center'},
+  {y: 1-0.26412763142448, x: 1.101473, country: 'CAN', labelY: 'center', labelX: 'right'},
+  {y: 1-0.454004393313688, x: 1.109881, country: 'FRA', labelY: 'center', labelX: 'right'},
+  {y: 1-0.748764556327701, x: 1.169148, country: 'US', labelY: 'center', labelX: 'right'},
 ]; 
+
+
+
+
+
+
+
+
+
+
 
 
 
