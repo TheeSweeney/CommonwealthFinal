44c28bf6e65bde3df06df15ae7b5b12cf61f1d1a
Brian Sweeney <bsweeney930@gmail.com>
2017-05-21 16:09:04 -0400

    updated data for chart 3

--- data/chart3data.js
+++ data/chart3data.js
@@ -3,17 +3,17 @@ var elevenCountries = ' Score in Eleven Countries'
 var dataSet =  {
   dataOverall:{
     data: [//TODO Rename this
-      {country: "UK", value: 1.367418713525976, rank: 1 },
-      {country: "AUS", value: 1.357430028143254, rank: 2 },
-      {country: "NETH", value: 1.251787757074009, rank: 3 },
-      {country: "NZ", value: 1.151665180612727, rank: 4 },
-      {country: "NOR", value: 1.13376085186227, rank: 5 },
-      {country: "SWIZ", value: 1.0675352522899197, rank: 6 },
-      {country: "SWE", value: 1.0674114551491452, rank: 7 },
-      {country: "GER", value: 1.064104826992644, rank: 8 },
-      {country: "CAN", value: 1-0.247553882107696, rank: 9 },
-      {country: "FRA", value: 1-0.41705654741814, rank: 10 },
-      {country: "US", value: 1-0.706030660985181, rank: 11 }
+      {country: "UK", value: 1.37, rank: 1 },
+      {country: "AUS", value: 1.36, rank: 2 },
+      {country: "NETH", value: 1.27, rank: 3 },
+      {country: "NZ", value: 1.13, rank: 4 },
+      {country: "NOR", value: 1.13, rank: 5 },
+      {country: "SWIZ", value: 1.08, rank: 6 },
+      {country: "SWE", value: 1.08, rank: 7 },
+      {country: "GER", value: 1.07, rank: 8 },
+      {country: "CAN", value: 1-0.26, rank: 9 },
+      {country: "FRA", value: 1-0.45, rank: 10 },
+      {country: "US", value: 1-0.75, rank: 11 }
     ],
     title: 'Health System Performance' + elevenCountries
   },
@@ -38,14 +38,14 @@ var dataSet =  {
       {country: 'UK', value:  1.39, rank:  3},
       {country: 'AUS', value: 1.19, rank:  4},
       {country: 'NETH', value:  1.7, rank: 1},
-      {country: 'NZ', value:  1.14, rank:  5},
-      {country: 'NOR', value: 1.02, rank:  7},
-      {country: 'SWIZ', value:  1-0.11, rank: 8},
+      {country: 'NZ', value:  1.02, rank:  7},
+      {country: 'NOR', value: 1.14, rank:  5},
+      {country: 'SWIZ', value:  1-0.11, rank: 9},
       {country: 'SWE', value: 1.06, rank:  6},
       {country: 'GER', value: 1.58, rank:  2},
-      {country: 'CAN', value: 1-0.77, rank: 10},
-      {country: 'FRA', value: 1-0.15, rank: 9},
-      {country: 'US', value:  1-1.07, rank: 11}
+      {country: 'CAN', value: 1-0.77, rank: 11},
+      {country: 'FRA', value: 1-0.14, rank: 10},
+      {country: 'US', value:  1-1.07, rank: 8}
     ],
     title: 'Access to Healthcare' + elevenCountries
   },
@@ -54,8 +54,8 @@ var dataSet =  {
       {country: 'UK', value:  2.59, rank:  3},
       {country: 'AUS', value: 2.74, rank:  1},
       {country: 'NETH', value:  2-0.15, rank: 9},
-      {country: 'NZ', value:  2.54, rank:  4},
-      {country: 'NOR', value: 2.6, rank: 2},
+      {country: 'NZ', value:  2.6, rank:  2},
+      {country: 'NOR', value: 2.54, rank: 4},
       {country: 'SWIZ', value:  2-0.12, rank: 8},
       {country: 'SWE', value: 2.26, rank:  5},
       {country: 'GER', value: 2.08, rank:  6},
@@ -67,17 +67,17 @@ var dataSet =  {
   },
   equityData:{
     data: [
-      {country: 'UK', value:  1-0.63, rank: 10},
-      {country: 'AUS', value: 1-0.14, rank: 7},
-      {country: 'NETH', value:  1.06, rank:  4},
-      {country: 'NZ', value:  1.39, rank:  2},
-      {country: 'NOR', value: 1-0.09, rank: 6},
-      {country: 'SWIZ', value:  1.32, rank:  3},
-      {country: 'SWE', value: 1.55, rank:  1},
-      {country: 'GER', value: 1.01, rank:  5},
-      {country: 'CAN', value: 1-0.39, rank: 8},
-      {country: 'FRA', value: 1-0.53, rank: 9},
-      {country: 'US', value:  1-0.76, rank: 11}
+      {country: 'UK', value:  1.93, rank: 1},
+      {country: 'AUS', value: 1-0.14, rank: 8},
+      {country: 'NETH', value:  1.46, rank:  2},
+      {country: 'NZ', value:  1.24, rank:  5},
+      {country: 'NOR', value: 1.14, rank: 6},
+      {country: 'SWIZ', value:  1.34, rank:  4},
+      {country: 'SWE', value: 1.37, rank:  3},
+      {country: 'GER', value: 1.01, rank:  7},
+      {country: 'CAN', value: 1-0.39, rank: 9},
+      {country: 'FRA', value: 1-0.53, rank: 10},
+      {country: 'US', value:  1-0.94, rank: 11}
     ],
     title: 'Equity' + elevenCountries
   },
@@ -85,13 +85,13 @@ var dataSet =  {
     data: [
       {country: 'UK', value:  1-0.63, rank: 10},
       {country: 'AUS', value: 1.62, rank:  1},
-      {country: 'NETH', value:  1.06, rank:  6},
-      {country: 'NZ', value:  1.39, rank:  3},
-      {country: 'NOR', value: 1-0.09, rank: 7},
+      {country: 'NETH', value:  1.03, rank:  6},
+      {country: 'NZ', value:  1-0.12, rank:  7},
+      {country: 'NOR', value: 1.42, rank: 3},
       {country: 'SWIZ', value:  1.32, rank:  4},
       {country: 'SWE', value: 1.55, rank:  2},
       {country: 'GER', value: 1-0.18, rank: 8},
-      {country: 'CAN', value: 1-0.39, rank: 9},
+      {country: 'CAN', value: 1-0.35, rank: 9},
       {country: 'FRA', value: 1.23, rank:  5},
       {country: 'US', value:  1-0.76, rank: 11}
     ],
